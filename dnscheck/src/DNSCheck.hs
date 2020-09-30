edb4780142e94af449e5da2b00e629c085382cfa
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-09-30 11:02:53 +0200

    Support for ipv6 checks

--- dnscheck/src/DNSCheck.hs
+++ dnscheck/src/DNSCheck.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module DNSCheck
   ( dnsCheck,
@@ -10,10 +11,10 @@ module DNSCheck
 where
 
 import Control.Monad
-import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as SB8
 import Data.IP
 import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Yaml
 import GHC.Generics (Generic)
 import Network.DNS
@@ -37,21 +38,31 @@ checkSpec Spec {..} = do
   unless (all (== ResultOk) results) $ die "Some check(s) failed"
 
 checkCheck :: Check -> IO CheckResult
-checkCheck = \case
-  CheckA domain expectedIpv4 -> do
-    rs <- makeResolvSeed defaultResolvConf
-    withResolver rs $ \resolver -> do
-      errOrIps <- DNS.lookupA resolver domain
-      pure $ case errOrIps of
-        Left err -> ResultError err
-        Right ips ->
-          if expectedIpv4 `elem` ips
-            then ResultOk
-            else ResultAFailed domain expectedIpv4 ips
+checkCheck c = do
+  rs <- makeResolvSeed defaultResolvConf
+  withResolver rs $ \resolver ->
+    case c of
+      CheckA domain expectedIpv4 -> do
+        errOrIps <- DNS.lookupA resolver domain
+        pure $ case errOrIps of
+          Left err -> ResultError err
+          Right ips ->
+            if expectedIpv4 `elem` ips
+              then ResultOk
+              else ResultAFailed domain expectedIpv4 ips
+      CheckAAAA domain expectedIpv6 -> do
+        errOrIps <- DNS.lookupAAAA resolver domain
+        pure $ case errOrIps of
+          Left err -> ResultError err
+          Right ips ->
+            if expectedIpv6 `elem` ips
+              then ResultOk
+              else ResultAAAAFailed domain expectedIpv6 ips
 
 data CheckResult
   = ResultError DNSError
   | ResultAFailed Domain IPv4 [IPv4]
+  | ResultAAAAFailed Domain IPv6 [IPv6]
   | ResultOk
   deriving (Show, Eq, Generic)
 
@@ -67,6 +78,7 @@ instance FromJSON Spec where
 
 data Check
   = CheckA Domain IPv4
+  | CheckAAAA Domain IPv6
   deriving (Show, Eq, Generic)
 
 instance FromJSON Domain where
@@ -75,8 +87,13 @@ instance FromJSON Domain where
 instance FromJSON IPv4 where
   parseJSON = fmap read . parseJSON
 
+instance FromJSON IPv6 where
+  parseJSON = fmap read . parseJSON
+
 instance FromJSON Check where
   parseJSON = withObject "Check" $ \o -> do
     t <- o .: "type"
     case (t :: Text) of
       "a" -> CheckA <$> o .: "domain" <*> o .: "ip"
+      "aaaa" -> CheckAAAA <$> o .: "domain" <*> o .: "ip"
+      _ -> fail $ "Unknown dns record type: " <> T.unpack t
